c237b2c7be9c5735adcabcea9af51a8539446be5
issamkhbou <44786964+issamkhbou@users.noreply.github.com>
2022-06-17 02:33:39 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,16 +1,4 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t issam97docker/jenkins_test:JMA2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push issam97docker/jenkins_test:JMA2.0'
-    }
-} 
 
 def deployApp() {
     echo 'deploying the application...'
